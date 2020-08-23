d25a0bfc16fc566d24c547ad7b957bd8da9a94f1
kamppox123 <kpoczatko71@interia.pl>
2020-08-23 15:44:09 +0200

    First Trial Activity

--- MainProject/app/src/main/java/com/example/mainframe/MainActivity.kt
+++ MainProject/app/src/main/java/com/example/mainframe/MainActivity.kt
@@ -1,39 +1,23 @@
 package com.example.mainframe
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
+        button_go.setOnClickListener {
+            button_go.setText("razdwa")
+            var message =Toast.makeText(applicationContext, "Dzie", Toast.LENGTH_LONG)
+            message.show()
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
+
+
